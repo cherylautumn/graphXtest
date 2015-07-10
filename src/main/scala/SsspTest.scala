b440d377112a04afe3b1bbeea3959574f56507db
cherylautumn <cherylautumn@gmail.com>
2015-07-09 17:25:26 -0700

    fix sssp

--- src/main/scala/SsspTest.scala
+++ src/main/scala/SsspTest.scala
@@ -3,6 +3,15 @@ package main.scala
 /**
  * Created by cheryl on 2015/7/9.
  */
+import org.apache.spark.graphx._
+// Import random graph generation library
+import org.apache.spark.graphx.util.GraphGenerators
+// A graph with edge attributes containing distances
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
 object SsspTest {
   def main(args: Array[String]) {
 
@@ -21,11 +30,11 @@ object SsspTest {
     val graph = Graph(users, edges, "").cache()
 
 
-    val graph: Graph[Int, Double] =
-      GraphGenerators.logNormalGraph(sc, numVertices = 100).mapEdges(e => e.attr.toDouble)
-    val sourceId: VertexId = 42 // The ultimate source
+//    val graph: Graph[Int, Double] =
+//      GraphGenerators.logNormalGraph(sc, numVertices = 100).mapEdges(e => e.attr.toDouble)
+//    val sourceId: VertexId = 42 // The ultimate source
     // Initialize the graph such that all vertices except the root have distance infinity.
-    val initialGraph = graph.mapVertices((id, _) => if (id == sourceId) 0.0 else Double.PositiveInfinity)
+    val initialGraph = graph.mapVertices((id, _) => if (id == users) 0.0 else Double.PositiveInfinity)
     val sssp = initialGraph.pregel(Double.PositiveInfinity)(
       (id, dist, newDist) => math.min(dist, newDist), // Vertex Program
       triplet => {  // Send Message
